343ae6a03df950b05a63067003d10a9e6c0ba998
Zhang Guotao <zhangguotao@outlook.com>
2016-10-20 16:56:44 +0800

    修改

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,8 +2,13 @@ from django.conf.urls import  include, url
 from . import views
 
 urlpatterns = [
+    #url(r'^$', views.post_list),
+    #url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
+    #url(r'^post/new/$', views.post_new, name='post_new'),
+
     url(r'^$', views.post_list),
-    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
+   # url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
+    #url(r'^post/new/$', views.post_new, name='post_new'),
 ]
 '''
 from django.conf.urls import url
